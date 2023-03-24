b933f300dea37bfcf82ced236bc0cbf6ae02c24f
Joesteph-L <nguyen.ly98se@gmail.com>
2023-03-24 17:04:32 +0700

    update boostrap 5, update function login logout

--- Libanon/Controllers/AccountController.cs
+++ Libanon/Controllers/AccountController.cs
@@ -99,7 +99,6 @@ namespace Libanon.Controllers
                             returnUrl = "/Home";
                         }
                         return RedirectToLocal(returnUrl);
-
                     }
                     else
                     {
